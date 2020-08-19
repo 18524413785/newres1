85c4180f4cf7cbbc544ed0cae65cc65e3299b7e8
v <Administor>
2020-08-19 15:44:08 +0800

    第一次修改

---
+++ demo/src/com/neuedu/day01/Calculate.java
@@ -0,0 +1,37 @@
+package com.neuedu.day01;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 435234+23523
+		int x = 314123;
+		// 变量类型 变量名=值
+		int y = 23;
+		int z = x + y;
+		System.out.println("计算结果为："+z);
+		
+		float x1=0.12f;//赋值时后面加f
+		float x2=2.4F;
+		float x3=x1*x2;
+		
+		System.out.println("计算结果为："+x3);
+		
+		char q1='你';
+		char q2='7';
+		System.out.println(q2);
+		
+		boolean w1=true;
+		boolean w2=false;
+		System.out.println(w1);
+		short p3=32767;
+		int p5=400000000;
+		int p4=p3*p5;
+		System.out.println(p4);
+		
+		byte r1=-128;
+		
+
+	}
+
+}
---
+++ demo/src/com/neuedu/day01/Test01.java
@@ -0,0 +1,17 @@
+package com.neuedu.day01;
+
+public class Test01 {
+
+	public static void main(String args[]) { 
+		int i = 0, total = 0; 
+		do {
+		 i = i + 2;
+		total = total + (i++) * 2; } 
+		while (i < 12); 
+		System.out.print("总数为：" + total); 
+		System.out.println("world");
+		System.out.println("world");
+		System.out.println("hello");}
+
+
+}
